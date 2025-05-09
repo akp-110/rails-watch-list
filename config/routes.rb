00620d3b23b62530fd6a9ba01acb7f057c545b5f
Anil Patel <patelak1@gmail.com>
2025-05-09 13:03:57 +0100

    adding view files

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: 'movies#index'
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
